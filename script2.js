f5a20fc54330797f8b6322b7780f9ec102885028
Aazim Sadan <aazimsadan99@gmail.com>
2024-08-12 01:19:36 +0530

    commit xd

---
+++ script2.js
@@ -0,0 +1,23 @@
+// Data types of values
+
+// 7 Data types
+
+// Numbers = 23, 324, 23, 23;
+
+// Strings "Aazim" , "23" , 'Also a Srting'
+
+// Boolean true/false
+
+// Null Intentionally absense of value
+// let marks = null;
+// marks = 56;
+// console.log(marks)
+
+// Undefined Variable declared but not initialized
+// let points;
+// points = 22;
+// console.log(points)
+
+// Object Complex data structure
+
+
