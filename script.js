40d36621ed2c4ec7f638f20740c1dafdb259d3d1
Aazim Sadan <aazimsadan99@gmail.com>
2024-08-12 00:48:34 +0530

    rename file

--- script.js
+++
@@ -1,5 +0,0 @@
-// Variables, Constants And Comments
-
-var score = 34;
-var score = 45;
-console.log(score)
---
+++ script1.js
@@ -0,0 +1,12 @@
+// Variables, Constants And Comments
+
+var score = 34;
+var score = 45;
+console.log(score)
+
+
+let points = 67;
+console.log(points)
+
+
+// Constants
