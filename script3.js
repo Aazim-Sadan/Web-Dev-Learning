29d1bf9e5f71c1dcf81adce9085cd3ea6b7ee209
Aazim Sadan <aazimsadan99@gmail.com>
2024-08-12 01:40:49 +0530

    commit string

---
+++ script3.js
@@ -0,0 +1,19 @@
+// This is String
+console.log("Hello Its me Aazim")
+let firstname = "Aazim"
+let lastname = "Sadan"
+// console.log(firstname,lastname)
+
+// String Concatination
+
+// Method 1 using + operator
+
+// console.log(firstname + " " + lastname)
+
+// Method 2 using template literals
+
+let fullName = `My name is ${firstname} ${lastname}`
+console.log(fullName)
+
+// Getting string character
+console.log(firstname[0])
