b086b6a6e3589b440443abc19ac05451ffd06041
Aazim Sadan <aazimsadan99@gmail.com>
2024-08-13 01:45:54 +0530

    Array properties

--- script6.js
+++ script6.js
@@ -9,4 +9,19 @@ console.log(phones[2])
 // Modify the Array
 
 phones[0] = "Vivo";
-console.log(phoens)
+console.log(phones)
+
+// Array Methods 
+
+// Join Methods
+
+console.log(phones.join(" "))
+
+// indexOf
+
+console.log(phones.indexOf("samsung"))  //case Sensitive
+
+// concat Method
+
+let iPhoneModel = ["13pro", "14promax", "12pro"]
+console.log(phones.concat(iPhoneModel))
