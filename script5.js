2ad46ee76290b0be5d5679b51acdf5c565de06b4
Aazim Sadan <aazimsadan99@gmail.com>
2024-08-13 00:17:33 +0530

    equality operators

---
+++ script5.js
@@ -0,0 +1,36 @@
+// Numbers
+
+let score = 50;
+// console.log(score , typeof score)
+
+// Mathematical Operators * / % + - ** 
+
+// let result = score / 2
+
+// console.log(result)
+
+// Mathematical Expressions
+
+let result = score *2 + (3+4) / 3 - 8 
+
+console.log(result);
+
+// Concatination of Numbers
+
+let marksline = ("My total marks is =" + result)
+console.log(marksline)
+
+
+
+
+// Loose Equality (==) VS Strict Equality Operator
+
+// Loose Equality Operator
+
+let age = 24; // Number type value
+console.log(age == "24") // Focus only on value not on type
+
+// Strict Equality Operator
+
+console.log(age === "24")
+
