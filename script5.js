10231962cda44578e4811f9253a0d204b6c61bbb
Aazim Sadan <aazimsadan99@gmail.com>
2024-08-13 00:53:02 +0530

    commit boolean

--- script5.js
+++ script5.js
@@ -9,6 +9,7 @@ let score = 50;
 
 // console.log(result)
 
+
 // Mathematical Expressions
 
 let result = score *2 + (3+4) / 3 - 8 
@@ -21,17 +22,55 @@ let marksline = ("My total marks is =" + result)
 console.log(marksline)
 
 
+// ========================================================================================================
 
 
 // Loose Equality (==) VS Strict Equality Operator
 
 // Loose Equality Operator
 
-let age = 24; // Number type value
-console.log(age == "24") // Focus only on value not on type
+// let age = 24; // Number type value
+// console.log(age == "24") // Focus only on value not on type
 
-// Strict Equality Operator
+// // Strict Equality Operator
 
-console.log(age === "24") // Focus on both value and type
+// console.log(age === "24") // Focus on both value and type
 
 
+
+// =========================================================================================================
+
+
+
+// Type Conversion
+
+// let stringTyoe = "50";
+
+// console.log(stringTyoe, typeof stringTyoe)
+
+// Number Method
+
+// let numberType = Number(stringTyoe)
+// console.log(numberType, typeof numberType)
+
+// Imp Note
+// In number method passing String must be numeric value
+
+let numberType = 67;
+
+// String Method
+
+let stringTyoe = String(numberType)
+console.log(stringTyoe, typeof stringTyoe)
+
+
+// =================================================================================
+
+// Boolean Method True/False
+
+let abc = ""; // Emptly strings are false
+
+let age = 32;
+
+let booleanValueOfAge = Boolean(age);
+console.log("Boolean Value of age is", booleanValueOfAge)
