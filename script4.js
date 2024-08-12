f892269ebf7bacb82a278863559c05632d4de005
Aazim Sadan <aazimsadan99@gmail.com>
2024-08-12 23:49:42 +0530

    slice + split

--- script4.js
+++ script4.js
@@ -1,3 +1,22 @@
 // String Slice method
 
 let fullname = "webdeveloper"
+console.log(fullname.slice(0,5)) // Excluding last one 0 to 4
+
+
+// String Split method
+
+let favPhones = "Iphone Samsung Vivo Oneplus"
+
+let arrPhone = favPhones.split(' ')
+// string to array
+
+console.log(arrPhone)
+
+
+// JavaScript strings are immutable
+
+let str = "Hello"
+
+// console.log(str)
+
