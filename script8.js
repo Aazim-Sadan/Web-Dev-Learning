1f7bd8742fcd78ee9a61c9b1a6ec9000f8ad1dae
Aazim Sadan <aazimsadan99@gmail.com>
2024-08-14 00:11:38 +0530

    For loop practice

--- script8.js
+++ script8.js
@@ -1,3 +1,22 @@
 // Control flow
 // For Loop
 
+// Run the specific task 5 times
+
+// for(let i=1; i<=10; i++){
+//     console.log("Run the task", i);
+    
+// }
+
+
+// Usecase of For loop
+
+let mobile = ["Samsung", "iPhone", "Oneplus", "Vivo", "Redmi", "Nothing"]
+
+for(let i = 0; i<mobile.length; i++){
+    console.log(mobile[i]);
+    
+}
+
+// iteration and value of i
+// 0 , 1
