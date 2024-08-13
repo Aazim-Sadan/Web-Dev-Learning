44a4c7ad52d7745ced1186696416e525c25d7533
Aazim Sadan <aazimsadan99@gmail.com>
2024-08-13 15:58:47 +0530

    commit boolean

---
+++ script7.js
@@ -0,0 +1,12 @@
+// Boolean Values and comparison Operators
+
+let booleanType = true;
+let stringType = "true";
+
+console.log(typeof booleanType, typeof stringType)
+
+// Methods return Boolean values
+
+let email = "webdev@example.com"
+let booleanValue = email.includes("@")
+console.log(booleanValue)
