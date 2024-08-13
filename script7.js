e6d6d76d291a98d11f9106c83bcfb47cbd6d3d27
Aazim Sadan <aazimsadan99@gmail.com>
2024-08-13 16:14:34 +0530

    comparison

--- script7.js
+++ script7.js
@@ -10,3 +10,18 @@ console.log(typeof booleanType, typeof stringType)
 let email = "webdev@example.com"
 let booleanValue = email.includes("@")
 console.log(booleanValue)
+
+
+// Comparison value always return Boolen values
+
+// ==, ===, !==, !=
+// >, >=
+// <, <=
+
+let points = 23;
+console.log(points == "23")
+console.log(points === "23")
+console.log(points > "12")
+console.log(points < "34")
+console.log(points != 23)
+
