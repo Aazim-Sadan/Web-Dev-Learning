2de883d8f9975f80429e645aa8cd96afd728c2f3
Aazim Sadan <aazimsadan99@gmail.com>
2024-08-14 22:58:21 +0530

    commit break-continue

---
+++ script10.js
@@ -0,0 +1,23 @@
+// Break and Cantinue statement
+
+// Also called jump statement
+
+// Cantinue statement
+
+// used to skip some values of loop
+
+for(let i = 1; i<=10; i++){
+if (i == 3) {
+    continue; // Moves towards the next iteration
+}console.log("Value of i = ", i)
+
+}
+
+
+
+// Break statement
+
+// Used to terminate the loop and controll transfer outside the loop
+
+
+
