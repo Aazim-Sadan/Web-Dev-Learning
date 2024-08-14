519f0274d0e690964e563092a4b3f5203c2d646e
Aazim Sadan <aazimsadan99@gmail.com>
2024-08-15 01:28:54 +0530

    adding switch in cant-brk

--- script12.js
+++ script12.js
@@ -33,3 +33,27 @@ if ( age > 18) {
 
 let result = age > 18 ? weight > 55 ? "Qualify" : "WeightIssue" : "Fail";
 console.log(result);
+
+
+// Switch statement
+
+let age1 = 17;
+
+switch(age1){
+    case 15:
+        case 16:
+            case 17:
+                result = "These ages are allowed to sit in the exam"
+                break;
+
+                default:
+                    result = "Not Allowed"
+
+}
+// If there is no break statement then "next" case will be executed
+console.log(result);
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
