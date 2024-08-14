f916578cc96e02f8082458a5dba5d6c527a30eaa
Aazim Sadan <aazimsadan99@gmail.com>
2024-08-14 15:26:55 +0530

    if, ifelse

---
+++ script9.js
@@ -0,0 +1,18 @@
+// if , else , else if
+
+let Age = 19
+
+if (Age>=18) {
+    console.log("You can drive");
+    
+}
+
+// if else 
+
+let age = 12
+if (age<18) {
+    console.log("You can't drive");
+    
+} else {
+    console.log("You can drive")
+}
