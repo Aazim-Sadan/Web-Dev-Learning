45d67097c483ab1bffdf236d1ee0c33ef05bf37d
Aazim Sadan <aazimsadan99@gmail.com>
2024-08-14 16:40:47 +0530

    Nested-if

--- script9.js
+++ script9.js
@@ -2,17 +2,54 @@
 
 let Age = 19
 
-if (Age>=18) {
+if (Age >= 18) {
     console.log("You can drive");
-    
+
 }
 
 // if else 
 
 let age = 12
-if (age<18) {
+if (age < 18) {
     console.log("You can't drive");
-    
+
 } else {
     console.log("You can drive")
 }
+
+// elseif
+
+let budget = 8000
+if (budget > 6000) {
+    console.log("Will do dinner in 5 Star");
+}
+else if (budget > 4500) {
+    console.log("Will do dinner in 3 Star");
+}
+else if (budget > 3000) {
+    console.log("Will do dinner in 2 Star");
+}
+else {
+    console.log("Will do dinner later ");
+}
+
+// After first condition true control get out of scope
+
+
+
+// ============================================================
+
+
+// Nested-if statement
+
+let number = 34;
+if (number > 23) {
+    console.log("Run if first condition is true");
+    if (number > 66) {
+        console.log("Run if both condition is true");
+        if (number > 22) {
+            console.log("Run if all condition is true");
+        }
+    }
+}
+
