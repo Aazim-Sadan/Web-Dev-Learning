e4cd78d6200dd3d8564b18abbc5743e7b3bc8358
Aazim Sadan <aazimsadan99@gmail.com>
2024-08-14 15:00:22 +0530

    while-loop

--- script8.js
+++ script8.js
@@ -33,8 +33,24 @@ for(let i = 1; i<=10; ++i){
 
 // While Loop
 
-for(let i = 1; i<5; i++){
+for(let i = 1; i<=5; i++){  // Infinite loop in case we miss conditional statement
     console.log("For Loop", i)
 }
 
-while()
+// while()
+
+let j = 1
+while (j<=5) { // Get error >-- Conditional statement is mandatory
+    console.log("While Loop", j);
+    j++
+    
+}
+
+let friends = ["Aazim", "Faizan", "Faisal", "Golden"]
+
+let k = 0;
+while (k<friends.length) {
+    console.log("Testing", friends[k])
+    k++ // WIthout this statement we will get infinite loop
+}
+
