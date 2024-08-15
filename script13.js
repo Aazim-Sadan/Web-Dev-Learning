94f5cd567142d025a976f3c4d850712f8dbf5bb6
Aazim Sadan <aazimsadan99@gmail.com>
2024-08-15 23:41:27 +0530

    Arguments and Parameter

--- script13.js
+++ script13.js
@@ -20,10 +20,44 @@ nameOfFunction()
 
 // Function Expression
 
-let func = function(){ // Also called anonymous function
+let func = function () { // Also called anonymous function
+    console.log("Example of function expression");
 
 }
 
+// Calling function expression
+
+func()
 
 
+
+// Paasing Values (Arguments and Parameters)
+
+let invitation = function (name = "defaultName", time = "night") { // Local variable to this function
+    console.log(`Welcome! ${name} You are are invited in our event at ${time}`);
+
+}
+
+invitation("Faizan", "morning")
+invitation()
+invitation("Golden")
+
+
+
+
+// Returning Values (How to return value from the function)
+
+let ageCalculator = function (birthYear, currentYear = 2024) {
+    let age = currentYear - birthYear
+    // console.log(`Your age is = ${age}`);
+
+    return age;
+
+    // console.log("After return statement no code will be executed");
+
+}
+
+// console.log(ageCalculator(1999));
+let ageResult = ageCalculator(1999);
+console.log(`Your age is = ${ageResult}`);
 
