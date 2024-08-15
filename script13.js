f3d4c2694be8116ea5f2bed7687ec472cfa257dd
Aazim Sadan <aazimsadan99@gmail.com>
2024-08-15 22:52:51 +0530

    function expression

--- script13.js
+++ script13.js
@@ -1,4 +1,29 @@
 // Functions
 
-// It is a piece of code that simply run in our program many times
+// It is simply a piece of code which can use in our program many times
+
+// It is just like a variable holding some piece of code
+
+
+
+// Declaration
+
+function nameOfFunction() {
+    console.log("We are writing code present inside the function");
+
+}
+
+// Function call
+
+nameOfFunction()
+
+
+// Function Expression
+
+let func = function(){ // Also called anonymous function
+
+}
+
+
+
 
