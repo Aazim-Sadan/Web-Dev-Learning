47cd5693c5493270a19d6bc54943fe5157613095
Aazim Sadan <aazimsadan99@gmail.com>
2024-08-16 16:16:09 +0530

    IIFE

---
+++ script15.js
@@ -0,0 +1,12 @@
+// Function
+
+// Immediately invoked function Expression
+// Executed only once
+
+// Now this is expression 
+(function(name){
+    console.log("This function will execute again", name);
+    
+})
+("IIFE")
+
