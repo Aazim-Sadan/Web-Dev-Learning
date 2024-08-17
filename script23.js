474937ec20edfe20dbe9271031059ad97484b31f
Aazim Sadan <aazimsadan99@gmail.com>
2024-08-17 23:48:50 +0530

    call, apply and bind

--- script23.js
+++ script23.js
@@ -20,3 +20,47 @@ flight.book(432, "Nawaz")
 flight.book(897, "Faisal")
 
 console.log(flight);
+
+
+let secondFlight = {
+    airline: "Air India",
+    iatacode: "AI",
+    bookings: [],
+}
+
+let book = flight.book;
+
+// book(847, "Lara") bbok is regular function 
+// "this" value is "undefined" at least in strict mode 
+
+
+// Solution Using "call" method 
+
+book.call(secondFlight, 349, "Lara");
+
+console.log(secondFlight);
+
+
+
+// Apply method 
+
+book.apply(secondFlight, [398, "Arshad"])
+console.log(secondFlight);
+
+
+// bind mehtod 
+// this method binds functions() with object 
+
+function greet(){
+    console.log(`Welcome ${this.firstName} ${this.secondName} to my Office`);
+    
+}
+
+let user = {
+    firstName: "Adnan",
+    secondName: "Ahmad"
+}
+
+let greets = greet.bind(user);
+
+greets()
