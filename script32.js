257163663e7d7222f8459413c8d90f8daeebcd37
Aazim Sadan <aazimsadan99@gmail.com>
2024-08-19 23:14:03 +0530

    working with array

---
+++ script32.js
@@ -0,0 +1,27 @@
+// Working with Array (Simple Method)
+
+let letters = ["a", "b", "c", "d"];
+
+// Reverse (Mutate the original array)
+
+let result = letters.reverse();
+
+console.log(result)
+console.log(letters);
+
+
+
+// concat 
+
+let num = [1,2,3,4]
+let concated = num.concat(letters)
+
+console.log(concated)
+
+
+// Join
+
+let joined = letters.join("-")
+console.log(joined)
+let index = letters.indexOf('b')
+console.log(index);
