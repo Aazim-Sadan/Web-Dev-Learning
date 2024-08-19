3bb4d2742f6ec0a7b11fdd069ef858c394ca9122
Aazim Sadan <aazimsadan99@gmail.com>
2024-08-20 00:22:12 +0530

    at Method

---
+++ script35.js
@@ -0,0 +1,20 @@
+// at Method 
+
+let num = [34,64,87,23]
+
+console.log(num[0]);
+console.log(num.at(1))
+
+// Getting last elements of array 
+
+console.log(num[num.length-1]);
+console.log(num.slice(-1)[0])
+console.log(num.at(-2))
+
+// at method are also used in strings 
+
+let car = "Creta"
+
+console.log(car.at(-1));
+
+
