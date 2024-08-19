4a7ab160d8aff11316606b72b882c7378321ce12
Aazim Sadan <aazimsadan99@gmail.com>
2024-08-19 23:27:33 +0530

    slice

---
+++ script33.js
@@ -0,0 +1,11 @@
+// Array Methods 
+
+// Slice Methods
+
+// used to Extract part of Array 
+// Array.slice(startIndex, lastIndex(exculuded))
+// Return new Array of extacted elements  
+
+let num = [2,5,7,8,5]
+let extractedArray = num.slice(2,4)
+console.log(extractedArray);
