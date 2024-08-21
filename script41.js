2f73bb2a72742f872cbc443b03792b28d8750b0b
Aazim Sadan <aazimsadan99@gmail.com>
2024-08-22 01:38:42 +0530

    flatMap

--- script41.js
+++ script41.js
@@ -13,3 +13,38 @@ let result = arr.flat(2) // by default 1(depth value) value
 
 console.log("After flat", result);
 
+
+
+// ============================================================
+
+// flatMap method 
+
+// It is the combination of the Map() method 
+// followed by the flat() method of the depth 1
+
+
+let cart = [{
+    name: "Mobile Phone",
+    qty: 2,
+    price: 10000,
+
+},
+{
+    name: "Tab",
+    qty: 1,
+    price: 8000
+}]
+
+let newCart = cart.flatMap(item =>{
+    if(item.name === "Mobile Phone"){
+        return [item,{
+            name:"Screen Protector",
+            qty:1,
+            price:0
+        }]
+    }else{
+        return [item]
+    }
+})
+
+console.log(newCart);
