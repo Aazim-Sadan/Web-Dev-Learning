3d8120589d3c00218c24f2b71de16309cac41db8
Aazim Sadan <aazimsadan99@gmail.com>
2024-08-27 01:14:14 +0530

    Short circuiting

--- script64.js
+++ script64.js
@@ -1,17 +1,6 @@
 // Short Circuiting (&&||
 
 
-let hotel = {
-    name: "Taj",
-    Location: "xyz road, New Delhi",
-    catogaries: ["Chinese", "Italian", "English"],
-    mainMenu: ["Food1", "Food2", "Food3"],
-    openingHour: {
-        sunday: { open: "09:00AM", close: "11:00PM" },
-        monday: { open: "10:00AM", close: "10:00PM" }
-    }
-}
-
 // False values => 0,false,undefined,null, ''(empty string) 
 // AND (&&) operator (Outut true is all input true) 
 
