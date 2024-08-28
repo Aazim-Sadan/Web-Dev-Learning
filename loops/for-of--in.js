301741cda97652f4318faae59d4c0df3711d166b
Aazim Sadan <aazimsadan99@gmail.com>
2024-08-28 16:19:09 +0530

    for-in and of

---
+++ loops/for-of--in.js
@@ -0,0 +1,65 @@
+// for of 
+
+
+let languages = ["React", "Node", "JavaScript", "TypeScript"]
+
+for (let lang of languages) {
+    console.log(lang);
+
+}
+
+
+let greeting = "Hello World"
+
+for (let greet of greeting) {
+    // console.log(`Each char is ${greet}`);
+
+}
+
+
+// uging for-of loop in Map()
+
+let map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+
+// console.log(map);
+
+
+for (let [key, value] of map) {
+    console.log(key, '-> ', value);
+}
+
+
+
+
+// ======================================================================
+
+// For-in loop
+
+
+let lang = {
+    js: 'JavaScript',
+    cpp: 'C++',
+    rb: 'Ruby',
+    swift: 'Swift by Apple'
+
+}
+
+for (const key in lang) {
+    console.log(`${key} shortcut is for ${lang[key]}`);
+    
+}
+
+
+
+// for-it loop in Array 
+
+let language = ["Js", "C++", "Py", "Java"]
+
+for (const key in language) {
+    console.log(language[key]);
+
+}
+
