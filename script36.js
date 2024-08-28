8be2148e6b91837fa9c07014cbace618c5074d79
Aazim Sadan <aazimsadan99@gmail.com>
2024-08-28 20:22:26 +0530

    Map

--- script36.js
+++ script36.js
@@ -3,10 +3,10 @@
 // Create new array from Original array by applying
 // Transformation function 
 
-let salaries = [4000,6000,9000,3000,5000]
+let salaries = [4000, 6000, 9000, 3000, 5000]
 
-let newSalaries = salaries.map(salary =>{
-    let incrementedAmount = salary/2;
+let newSalaries = salaries.map(salary => {
+    let incrementedAmount = salary / 2;
     return salary + incrementedAmount
 })
 
@@ -14,3 +14,17 @@ console.log("After 50% incrementation new salaries are", newSalaries);
 
 
 // Langth of new array always equal to the original array 
+
+
+
+// =============================================================================
+
+
+let myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+let newNums = myNumbers
+    .map((num) => num * 10)
+    .map((num) => num + 1)
+    .filter((num) => num > 40)
+
+console.log(newNums)
--- script37.js
+++ script37.js
@@ -37,10 +37,10 @@ const books = [
 ];
 
 
-let userBooks = books.filter((bk)=> bk.genre === "History" )
+let userBooks = books.filter((bk) => bk.genre === "History")
 
-userBooks = books.filter((bk)=> {
-  return  bk.publish >= 1995 && bk.genre === "History"
+userBooks = books.filter((bk) => {
+    return bk.publish >= 1995 && bk.genre === "History"
 })
 
 console.log(userBooks);
