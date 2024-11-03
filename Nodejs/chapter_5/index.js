ca50a611b67bf34f5c4565c260f8bdda0b2bbbc0
Aazim Sadan <aazimsadan99@gmail.com>
2024-11-03 22:10:53 +0530

    updating database with mongoose

--- Nodejs/chapter_5/index.js
+++ Nodejs/chapter_5/index.js
@@ -1,19 +1,29 @@
-require('dotenv').config({path:"../.env"});
+require('dotenv').config({ path: "../.env" });
 const express = require('express');
 const morgan = require('morgan');
 const server = express();
-const productRouter = require('./routes/product')
-const userRouter = require('./routes/user')
+const productRouter = require('./routes/product');
+const userRouter = require('./routes/user');
+const mongoose = require('mongoose');
 
-console.log('env', process.env.DB_PASSWORD);
+
+// DB Connection 
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/ecommerce');
+  console.log('Database connected');
+
+}
+
 
 
 //bodyParser
 server.use(express.json());
 server.use(morgan('default'));
 server.use(express.static('public'));
-server.use('/products',productRouter.router);
-server.use('/users',userRouter.router);
+server.use('/products', productRouter.router);
+server.use('/users', userRouter.router);
 
 server.listen(8080, () => {
   console.log('server started');
