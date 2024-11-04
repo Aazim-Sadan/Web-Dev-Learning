91c911e96fcb715d78bfd419d8ca58458ee27338
Aazim Sadan <aazimsadan99@gmail.com>
2024-11-04 23:02:30 +0530

    connecting API with React

--- Nodejs/chapter_5/index.js
+++ Nodejs/chapter_5/index.js
@@ -2,10 +2,11 @@ require('dotenv').config({ path: "../.env" });
 const express = require('express');
 const morgan = require('morgan');
 const server = express();
+const path = require('path');
 const productRouter = require('./routes/product');
 const userRouter = require('./routes/user');
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 
 // DB Connection 
 main().catch(err => console.log(err));
@@ -19,11 +20,15 @@ async function main() {
 
 
 //bodyParser
+server.use(cors());
 server.use(express.json());
-server.use(morgan('default'));
+server.use(express.static(path.resolve(__dirname,process.env.PUBLIC_DIR)));
 server.use(express.static('public'));
 server.use('/products', productRouter.router);
 server.use('/users', userRouter.router);
+server.use('*',(req,res)=>{
+  res.sendFile(path.resolve(__dirname,'build','index.html'))
+})
 
 server.listen(8080, () => {
   console.log('server started');
