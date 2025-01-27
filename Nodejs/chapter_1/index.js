633db3e2638f753afad5d27d74e1b8aac4151708
Aazim Sadan <aazimsadan99@gmail.com>
2025-01-27 19:20:41 +0530

    fs and os operations in nodejs

--- Nodejs/chapter_1/index.js
+++ Nodejs/chapter_1/index.js
@@ -1,14 +1,42 @@
 // file module 
 
 const fs = require('fs');
-const expess = require('express')
+const http = require('http')
+const os = require('os')
+
+console.log(os.cpus().length)
 
 
-
-
-const text = fs.readFileSync('demo.txt', 'utf-8', (err, text) => {
-    console.log(text);
-
+// Non-blocking request 
+fs.readFile("demo.txt", "utf-8", (err, result) => {
+    console.log(result)
 })
 
-console.log(text);
+
+const text = fs.readFileSync('demo.txt', 'utf-8')
+// console.log(text)
+
+const contact = fs.writeFileSync("myContact.txt", "Hey there, Its me Aazim")
+
+fs.cpSync("myContact.txt", "copy.txt")
+
+
+const myServer = http.createServer((req, res) => {
+    const log = `${Date.now()} ${req.url}: New Request recieved\n`
+    fs.appendFile("log.txt", log, () => {
+        switch (req.url) {
+            case "/":
+                res.end("Hello");
+            case "/about":
+                res.end("I am Aazim Sadan");
+                break;
+                default:
+                    res.end("404 - Not found");
+
+        }
+    })
+})
+
+myServer.listen(8000, () => console.log("Server listen at PORT 8000"))
+
+
